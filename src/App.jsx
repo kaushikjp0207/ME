3921bbd16276713a6bd44a70b881c49afb485f2b
Vani <vaaniparmar58@gmail.com>
2026-07-02 10:34:38 +0530

    Use official GitHub Pages deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import React from "react";
 import { motion, AnimatePresence } from 'framer-motion';
 import Lenis from 'lenis';
 import confetti from 'canvas-confetti';
